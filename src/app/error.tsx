fda0ce6b2d8fa1f5d9e773241116dcf816bd8c5d
MokoLaboratoire <desmoulin.federico@tutanota.com>
2023-11-14 09:38:44 +0000

    /error page

---
+++ src/app/error.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import React from 'react'
+
+export default function error({
+    error,
+    reset
+}: {
+    error: Error,
+    reset: () => void
+}) {
+  return (
+    <div>error</div>
+  )
+}
--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
+const session = null
+
 export default function Tasks() {
+
+	if(!session) throw new Error('Not authenticated')
 
 	console.log("Hello")
 
