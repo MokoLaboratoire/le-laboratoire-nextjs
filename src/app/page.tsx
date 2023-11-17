2f1488728537f8e0f79a7068a509cf11ef3a4e03
MokoLaboratoire <desmoulin.federico@tutanota.com>
2023-11-17 09:54:22 +0000

    app/page.tsx: clean

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 'use client'
 
-import React, { useEffect } from 'react'
-
-import { useGetTasksQuery } from '@/redux/api/taskApi'
+import React from 'react'
 
 export default function Home() {
   return (
