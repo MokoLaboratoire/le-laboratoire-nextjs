41d7d39025a802dbbabce26a619459ffc5a8bf4c
MokoLaboratoire <desmoulin.federico@tutanota.com>
2023-11-17 10:05:10 +0000

    r3f default cubes BUT alert with 'meshRef.current.rotation.x'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,40 @@
 'use client'
 
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+
+function Box(props: any) {
+
+  const meshRef = useRef()
+
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+
+  useFrame((state, delta) => {
+    if(meshRef && meshRef.current) (meshRef.current.rotation.x += delta)
+  })
+
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World!</h1>
-    </main>
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
   )
 }
