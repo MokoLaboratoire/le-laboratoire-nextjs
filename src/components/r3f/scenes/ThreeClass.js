d18aefd6bcb822356fd33419f3230aebe0dae6c0
MokoLaboratoire <desmoulin.federico@tutanota.com>
2024-01-30 19:14:25 +0100

    ThreeScene & ThreeClass restructured (Back to 16'11)

--- src/components/r3f/scenes/ThreeClass.js
+++ src/components/r3f/scenes/ThreeClass.js
@@ -6,8 +6,9 @@ export default class ThreeClass {
   constructor(props) {
     const { container } = props
 
-		this.time = 0
 		this.isPlaying = true
+		this.time = 0
+		this.size = 128
 
 		this.container = container
 
@@ -36,105 +37,39 @@ export default class ThreeClass {
 
     this.controls = new OrbitControls(this.camera, this.renderer.domElement)
 
-		/* this.render() */
+		this.addObjects()
+		this.setupFBO()
+		this.render()
+
 		this.setupResize()
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    this.size = 128
-
-		/* this.scene.background = new THREE.Color(0x000000) */
-
-
-
-
-    this.addParticules(this.scene)
-
-    /* this.fbo = this.getRenderTarget
-    this.fbol = this.getRenderTarget */
-    /* this.fboScene = new THREE.Scene()
-    this.fboCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, -1, 1)
-    this.fboCamera.position.set(0, 0, 0.5)
-    this.fboCamera.lookAt(0, 0, 0) */
-
-    /* container.appendChild(this.renderer.domElement) */
-
-    /* let geometry = new THREE.PlaneGeometry(2, 2) */
-
-    /* this.data = new Float32Array(this.size * this.size * 4)
-
-    for (let i = 0; i < this.size; i++) {
-      for (let j = 0; j < this.size; j++) {
-        let index = (i + j * this.size) * 4
-        let theta = Math.random() * Math.PI * 2
-        let r = 0.5 + 0.5 * Math.random()
-        this.data[index + 0] = r * Math.cos(theta)
-        this.data[index + 1] = r * Math.sin(theta)
-        this.data[index + 2] = 1.0
-        this.data[index + 3] = 1.0
-      }
-    }
-
-    this.fboTexture = new THREE.DataTexture(
-      this.data,
-      this.size,
-      this.size,
-      THREE.RGBAFormat,
-      THREE.FloatType,
-    )
-    this.fboTexture.magFilter = THREE.NearestFilter
-    this.fboTexture.minFilter = THREE.NearestFilter
-    this.fboTexture.needsUpdate = true */
-
-    /* this.fboMaterial = new THREE.ShaderMaterial({
-      uniforms: {
-        uPositions: { value: this.fboTexture },
-        time: { value: 0 },
+	addObjects() {
+    const geometry = new THREE.PlaneGeometry(1, 1)
+		const material = new THREE.ShaderMaterial({
+      extensions: {
+        derivatives: '#extension GL_OES_standard_derivatives : enable',
       },
-      vertexShader: `
- 				varying vec2 vUv;
+      side: THREE.DoubleSide,
+      uniforms: {
+			},
+			vertexShader: `
+				varying vec2 vUv;
 				void main() {
 					vUv = uv;
 					gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 				}
 			`,
 			fragmentShader: `
-				uniform sampler2D uPositions;
 				varying vec2 vUv;
 				void main() {
-					vec4 pos = texture2D(uPositions, vUv);
-					gl_FragColor = pos;
+					gl_FragColor = vec4(vUv, 0.0, 1.0);
 				}
 			`,
     })
-    this.fboMesh = new THREE.Mesh(geometry, this.fboMaterial)
-    this.fboScene.add(this.fboMesh) */
-  }
-
-	setupResize() {
-		window.addEventListener('resize', this.resize.bind(this))
-	}
-
-	resize() {
-		this.width = window.innerWidth
-		this.height = window.innerHeight
-		this.renderer.setSize(this.width, this.height)
-		this.camera.aspect = this.width / this.height
-		this.camera.updateProjectionMatrix()
+    this.plane = new THREE.Mesh(geometry, material)
+    this.plane.rotation.x = Math.PI / 4
+    this.scene.add(this.plane)
 	}
 
   getRenderTarget() {
@@ -151,61 +86,15 @@ export default class ThreeClass {
     return renderTarget
   }
 
-  addParticules(scene) {
-    this.material = new THREE.ShaderMaterial({
-      side: THREE.DoubleSide,
-      uniforms: {
-				uPositions: { value: null },
-        time: { value: 0 },
-      },
-      vertexShader: `
-        uniform sampler2D uPositions;
-        varying vec2 vUv;
-        void main() {
-          vUv = uv;
-          vec4 pos = texture2D(uPositions, vUv);
-          vec4 mvPosition = modelViewMatrix * vec4(pos.xyz, 1.0);
-          gl_PointSize = 10.0 * (1.0 / -mvPosition.z);
-          gl_Position = projectionMatrix * mvPosition;
-        }
-      `,
-      fragmentShader: `
-				uniform sampler2D uPositions;
-        varying vec2 vUv;
-        void main() {
-					vec4 pos = texture2D(uPositions, vUv);
-					pos.xy += vec2(0.1); 
-          gl_FragColor = pos;
-        }
-      `,
-    })
+	setupFBO() {
+		this.fbo = this.getRenderTarget()
+    this.fbo1 = this.getRenderTarget()
 
-    this.count = Math.pow(this.size, 2)
-
-    const geometry = new THREE.BufferGeometry()
-    const positions = new Float32Array(this.count * 3)
-    const uv = new Float32Array(this.count * 2)
-
-    for (let i = 0; i < this.size; i++) {
-      for (let j = 0; j < this.size; j++) {
-        let index = (i + j * this.size)
-        positions[index * 3 + 0] = Math.random()
-        positions[index * 3 + 1] = Math.random()
-        positions[index * 3 + 2] = 0
-        uv[index * 2 + 0] = i / this.size
-        uv[index * 2 + 1] = j / this.size
-      }
-    }
-
-    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
-    geometry.setAttribute('uv', new THREE.BufferAttribute(uv, 2))
-
-		this.fbo = this.getRenderTarget
-    this.fbo1 = this.getRenderTarget
-    this.fboScene = new THREE.Scene()
+		this.fboScene = new THREE.Scene()
     this.fboCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, -1, 1)
     this.fboCamera.position.set(0, 0, 0.5)
     this.fboCamera.lookAt(0, 0, 0)
+		let geometry = new THREE.PlaneGeometry(2, 2)
 
     this.data = new Float32Array(this.size * this.size * 4)
 
@@ -232,7 +121,7 @@ export default class ThreeClass {
     this.fboTexture.minFilter = THREE.NearestFilter
     this.fboTexture.needsUpdate = true
 
-    this.fboMaterial = new THREE.ShaderMaterial({
+		this.fboMaterial = new THREE.ShaderMaterial({
       uniforms: {
         uPositions: { value: this.fboTexture },
         time: { value: 0 },
@@ -254,28 +143,30 @@ export default class ThreeClass {
 			`,
     })
 
-    this.material.uniforms.uPositions.value = this.fboTexture
-    const points = new THREE.Points(geometry, this.material)
-    /* gsap.to(plane.rotation, {duration: 10, y: Math.PI * 2, repeat: -1, ease: "none"}); */
-    scene.add(points)
-  }
+    this.fboMesh = new THREE.Mesh(geometry, this.fboMaterial)
+    this.fboScene.add(this.fboMesh)
+	}
 
-  setAnimationLoop() {
-    this.renderer.setAnimationLoop((t) => {
-			this.material.uniforms.time.value = t
-			/* this.fboMaterial.uniforms.time.value = t */
+	render() {
+		if(!this.isPlaying) return
+		this.time += 0.05
+		/* this.material.uniforms.time.value = time */
+		requestAnimationFrame(this.render.bind(this))
+		this.renderer.render(this.scene, this.camera)
 
-			/* this.material.uniforms.uPositions.value = this.fbo.texture
-			this.fboMaterial.uniforms.uPositions.value = this.fbo1.texture */
+		/* this.renderer.setRenderTarget(null)
+		this.renderer.render(this.fboScene, this.fboCamera) */
+	}
 
-      /* this.renderer.setRenderTarget(this.fbo) */
-      this.renderer.render(this.scene, this.camera)
-      /* this.renderer.setRenderTarget(null)
-      this.renderer.render(this.fboScene, this.fboCamera) */
+	setupResize() {
+		window.addEventListener('resize', this.resize.bind(this))
+	}
 
-			let temp = this.fbo
-			this.fbo = this.fbo1
-			this.fbo1 = temp
-    })
-  }
+	resize() {
+		this.width = window.innerWidth
+		this.height = window.innerHeight
+		this.renderer.setSize(this.width, this.height)
+		this.camera.aspect = this.width / this.height
+		this.camera.updateProjectionMatrix()
+	}
 }
--- src/components/r3f/scenes/ThreeScene.tsx
+++ src/components/r3f/scenes/ThreeScene.tsx
@@ -12,36 +12,23 @@ export default class ThreeScene extends Component<{}, ThreeSceneState> {
 
   constructor(props: any) {
     super(props)
-    this.state = {
-      initialized: false,
-    }
     this.canvasRef = createRef()
   }
 
   componentDidMount() {
-    if (!this.state.initialized) {
-      this.init()
-    }
+    this.init()
   }
 
   init = () => {
-    const threeCanvas = new ThreeClass({
+    new ThreeClass({
       // @ts-ignore
       container: this.canvasRef.current,
     })
-    this.startRender(threeCanvas)
-    this.setState({ initialized: true })
-  }
-
-  startRender(threeClass: ThreeClass) {
-    console.log('threeClass', threeClass)
-    threeClass.setAnimationLoop()
   }
 
   render() {
     return (
       <div
-        /* style={{ width: window.innerWidth, height: window.innerHeight }} */
         ref={this.canvasRef}
       ></div>
     )
