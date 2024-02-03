6ebee5cef2a37848ee56ed6b12c188693ffb72a1
MokoLaboratoire <desmoulin.federico@tutanota.com>
2024-02-03 10:47:52 +0100

    next.config.js updated to handle .glsl files + vertexShader & fragmentShader exported

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* reactStrictMode: true,
-  swcMinify: true,
-  webpack: (config) => {
+  reactStrictMode: true,
+  webpack: (config, options) => {
     config.module.rules.push({
-      test: /\.(glsl|vs|fs|frag|vert)$/,
-      type: 'asset/source',
+      test: /\.glsl/,
+      type: "asset/source",
     })
     return config
-  }, */
+  },
 }
 
 module.exports = nextConfig
