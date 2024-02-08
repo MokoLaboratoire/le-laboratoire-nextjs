81c33ef51c104df14c52eafefa77af1d8ffaf05e
MokoLaboratoire <desmoulin.federico@tutanota.com>
2024-02-08 11:21:06 +0100

    Lines ball with random shape

--- src/components/r3f/scenes/DrawingPhotosWithLinesIn3DClass.js
+++ src/components/r3f/scenes/DrawingPhotosWithLinesIn3DClass.js
@@ -18,6 +18,10 @@ export default class DrawingPhotosWithLinesIn3DClass {
     this.width = window.innerWidth
     this.height = window.innerHeight
 
+    this.dots = 50
+    this.lines = 50
+    this.radius = 100
+
     this.renderer = new THREE.WebGLRenderer({
       alpha: true,
       antialias: true,
@@ -41,7 +45,31 @@ export default class DrawingPhotosWithLinesIn3DClass {
 
     this.controls = new OrbitControls(this.camera, this.renderer.domElement)
 
-    this.addObjects()
+    this.group = new THREE.Group()
+    this.scene.add(this.group)
+
+
+    this.material = new THREE.LineBasicMaterial({
+        color: 0xFF00FF
+    })
+
+    for(let i = 0; i < this.lines; i++) {
+        this.points = []
+
+        for(let j = 0; j < this.lines; j++) {
+            let coord = (j / this.dots) * this.radius * 2 - this.radius
+            this.points.push(new THREE.Vector3(coord, Math.random() * 30, 0))
+        }
+        
+        this.geometry = new THREE.BufferGeometry().setFromPoints(this.points)
+        this.line = new THREE.Line(this.geometry, this.material)
+        this.line.rotation.x = Math.random() * Math.PI
+        this.line.rotation.y = Math.random() * Math.PI
+        this.line.rotation.z = Math.random() * Math.PI
+        this.group.add(this.line)
+    }
+
+    /* this.addObjects() */
     this.render()
 
     this.setupResize()
@@ -65,8 +93,8 @@ export default class DrawingPhotosWithLinesIn3DClass {
 
   render() {
     if (!this.isPlaying) return
-    this.time += 0.05
-    this.material.uniforms.time.value = this.time
+    /* this.time += 0.05 */
+    /* this.material.uniforms.time.value = this.time */
     requestAnimationFrame(this.render.bind(this))
     this.renderer.render(this.scene, this.camera)
   }
