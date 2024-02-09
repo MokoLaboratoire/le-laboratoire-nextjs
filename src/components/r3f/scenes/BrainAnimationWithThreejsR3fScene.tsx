13b87441696fc05a6bdd7e12588bf06bd8d97f26
MokoLaboratoire <desmoulin.federico@tutanota.com>
2024-02-09 09:25:25 +0100

    Ball curves (34'30'')

--- src/components/r3f/scenes/BrainAnimationWithThreejsR3fScene.tsx
+++ src/components/r3f/scenes/BrainAnimationWithThreejsR3fScene.tsx
@@ -1,35 +1,142 @@
 'use client'
 
-import React, { Component, createRef, RefObject } from 'react'
-import BrainAnimationWithThreejsR3fClass from './BrainAnimationWithThreejsR3fClass.js'
+import React, { Suspense, useRef } from 'react'
+import * as THREE from 'three'
+import ReactThreeFiber, { Canvas, extend, useFrame } from '@react-three/fiber'
+import { OrbitControls, shaderMaterial } from '@react-three/drei'
 
-interface BrainAnimationWithThreejsR3fSceneState {
-  initialized: boolean
+import { data } from './data/brainAnimationWithThreejsR3f'
+
+const PATH = data.economics[0].paths
+
+const randonRange = (min: number, max: number) => Math.random() * (max - min) + min
+
+/* const Tube = () => {
+    const points = []
+    for(let i = 0; i < 10; i++) {
+        points.push(
+            new THREE.Vector3(
+                (i - 5) * 0.5,
+                Math.sin(i + 2) * 10 + 5,
+                0
+            )
+        )
+    }
+    const curve = new THREE.CatmullRomCurve3(points)
+
+    return (
+        <mesh>
+            <tubeGeometry args={[curve, 64, 0.1, 8, false]} />
+            <meshStandardMaterial color='hotpink' />
+        </mesh>
+    )
+} */
+
+interface CurveInterface {
+    curve: THREE.CatmullRomCurve3
 }
 
-export default class BrainAnimationWithThreejsR3fScene extends Component<
-  {},
-  BrainAnimationWithThreejsR3fSceneState
-> {
-  private canvasRef: RefObject<HTMLDivElement>
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            brainMaterial: ReactThreeFiber.Node<typeof BrainMaterial & JSX.IntrinsicElements['shaderMaterial'], typeof BrainMaterial>
+        }
+    }
+}
 
-  constructor(props: any) {
-    super(props)
-    this.canvasRef = createRef()
-  }
+const BrainMaterial = shaderMaterial(
+    { 
+        time: 0, 
+        color: new THREE.Color(0.2, 0.4, 0.1) 
+    },
+    /*glsl*/`
+      uniform float time;
+      varying vec2 vUv;
+      varying float vProgress;
+      void main() {
+        vUv = uv;
+        vProgress = smoothstep(-1.0, 1.0, sin(vUv.x * 8.0 + time));
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+    `,
+    /*glsl*/`
+      uniform float time;
+      uniform vec3 color;
+      varying vec2 vUv;
+      varying float vProgress;
+      void main() {
+        vec3 color1 = vec3(1.0, 0.0, 0.0);
+        vec3 color2 = vec3(1.0, 1.0, 0.0);
+        vec3 finalColor = mix(color1, color2, vProgress);
+        gl_FragColor.rgba = vec4(finalColor, 1.0);
+      }
+    `
+)
+extend({ BrainMaterial })
 
-  componentDidMount() {
-    this.init()
-  }
-
-  init = () => {
-    new BrainAnimationWithThreejsR3fClass({
-      // @ts-ignore
-      container: this.canvasRef.current,
+function Tube({ curve }: CurveInterface) {
+    const brainMatRef = useRef(null)
+    useFrame(({ clock }) => {
+        // @ts-ignore
+        brainMatRef.current.uniforms.time.value = clock.getElapsedTime();
     })
-  }
+    return (
+        <mesh>
+            <tubeGeometry args={[curve, 64, 0.01, 3, false]} />
+            <brainMaterial ref={brainMatRef} side={THREE.DoubleSide} />
+        </mesh>
+    )
+}
 
-  render() {
-    return <div ref={this.canvasRef}></div>
-  }
+function Tubes() {
+    const curves: THREE.CatmullRomCurve3[] = []
+    for(let i = 0; i < 100; i++) {
+        const points = []
+        const length = randonRange(0.1, 1)
+        for(let j = 0; j < 100; j++) {
+            points.push(
+                new THREE.Vector3().setFromSphericalCoords(
+                    1,
+                    Math.PI -(j / 100) * Math.PI * length,
+                    (i / 100) * Math.PI * 2
+                )
+            )
+        }
+        const tempcurve = new THREE.CatmullRomCurve3(points)
+        curves.push(tempcurve)
+    }
+    return (
+        <>
+            {curves.map((curve, index) => (
+                <Tube key={`curve_${index}`} curve={curve} />
+            ))}
+        </>
+    )
+}
+
+
+export default function BrainAnimationWithThreejsR3fScene() {
+  return (
+    <Canvas
+        id='canvas' 
+        shadows
+        legacy
+        gl={{
+            antialias: true,
+            alpha: true,
+            powerPreference: 'high-performance',
+            shadowMapEnabled: true
+        }}
+        camera={{ fov: 75, near: 0.1, far: 5000, position: [0, 0, 2] }}
+    >
+        <color attach='background' args={['black']} />
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <OrbitControls />
+        {/* <Suspense> */}
+            {/* <Tube /> */}
+            <Tubes />
+        {/* </Suspense> */}
+    </Canvas>
+  )
 }
